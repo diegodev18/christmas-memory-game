5888ff1612753e82e3528d525b50659a049d85d4
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-12-01 07:34:32 -0600

    feat: add Button component and update Home page layout with new buttons

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,22 @@
+import { Button } from "@/components/ui/button";
+
 function Home() {
   return (
-    <div className="mx-auto max-w-[800px]">
-      <h1>🎄 Christmas Memory Game</h1>
-      <p>Welcome to the Christmas Memory Game!</p>
-    </div>
+    <>
+      <div className="max-w-[800px] mx-auto pt-5">
+        <header className="flex justify-end gap-x-4">
+          <Button variant={"secondary"}>Start Game</Button>
+          <Button variant={"outline"}>Log in</Button>
+        </header>
+        <main className="mt-10">
+          <h1 className="text-center text-4xl font-medium">Memoristmas 🎄</h1>
+          <p className="text-center max-w-[450px] mx-auto mt-2">
+            Find all the matching pairs of Christmas-themed cards to win the
+            game!
+          </p>
+        </main>
+      </div>
+    </>
   );
 }
 
