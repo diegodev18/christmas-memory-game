a6724c8102193160e20d07c050e05c9493ecabb1
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-12-01 07:24:02 -0600

    fix: ensure image is not draggable in Background component

--- frontend/src/components/Background.tsx
+++ frontend/src/components/Background.tsx
@@ -1,6 +1,7 @@
 export default function Background() {
   return (
     <img
+      draggable={false}
       className="fixed top-0 left-0 size-full opacity-20 -z-50 object-center object-cover"
       src="/celebrate-merry-christmas-background.webp"
       alt=""
