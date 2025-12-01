e435a2fa7985b28481e949166a5e2d65b56c4150
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-12-01 07:14:39 -0600

    feat: add Background component with celebratory background image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import Home from "./pages/Home";
+import Background from "./components/Background";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <Background />
+    </>
   );
 }
 
---
+++ frontend/src/components/Background.tsx
@@ -0,0 +1,9 @@
+export default function Background() {
+  return (
+    <img
+      className="fixed top-0 left-0 size-full opacity-20 -z-50 object-center object-cover"
+      src="/celebrate-merry-christmas-background.webp"
+      alt=""
+    />
+  );
+}
