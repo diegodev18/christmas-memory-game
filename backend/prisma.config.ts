4ff1f20e91353a7dd9696ae43453dbaf36828bd7
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-12-08 10:31:25 -0600

    feat: integrate Prisma for database management and add PostgreSQL adapter

---
+++ backend/prisma.config.ts
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+import { defineConfig, env } from "prisma/config";
+
+config({ path: ".env" });
+
+export default defineConfig({
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+  schema: "prisma/schema.prisma",
+});
---
+++ backend/src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { DATABASE_URL } from "@/config";
+import { PrismaClient } from "@/generated/prisma/client";
+
+const adapter = new PrismaPg({ url: DATABASE_URL });
+
+export const prisma = new PrismaClient({ adapter });
+
+export default prisma;
