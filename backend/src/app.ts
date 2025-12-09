6529378e90bbf763355a97d0300d21f37fcd967a
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-12-08 21:10:44 -0600

    feat: update app configuration to include CORS, cookie parser, and session middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,21 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 
-import { PORT_NUMBER } from "@/config";
+import { CORS_OPTIONS, PORT_NUMBER } from "@/config";
+import { getSessionMiddleware } from "@/middlewares/session.middleware";
+import authRouter from "@/routes/auth.route";
 import cardsRouter from "@/routes/cards.route";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors(CORS_OPTIONS));
+app.use(getSessionMiddleware);
 
 app.use("/api/cards", cardsRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is running on port ${PORT_NUMBER.toString()}`);
