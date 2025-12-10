ae5ea4da5c14827a14280084f6fb4fa31b8b3e26
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-12-09 23:49:00 -0600

    refactor: restructure Auth component layout for improved readability

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -22,19 +22,21 @@ export default function Auth() {
   if (user) {
     return (
       <>
-        <Header />
-        <div className="flex flex-col gap-y-4 items-center justify-center h-screen">
-          <div className="text-lg font-mono">
-            <h1>Welcome, {user.user_name}!</h1>
-            <p>You are logged in.</p>
-          </div>
-          <div className="space-x-2">
-            <Button variant={"secondary"} onClick={() => navigate("/game")}>
-              Play now!
-            </Button>
-            <Button variant={"destructive"} onClick={logout}>
-              Logout
-            </Button>
+        <div className="max-w-[800px] mx-auto pt-5">
+          <Header />
+          <div className="flex flex-col gap-y-4 items-center justify-center h-[90vh]">
+            <div className="text-lg font-mono">
+              <h1>Welcome, {user.user_name}!</h1>
+              <p>You are logged in.</p>
+            </div>
+            <div className="space-x-2">
+              <Button variant={"secondary"} onClick={() => navigate("/game")}>
+                Play now!
+              </Button>
+              <Button variant={"destructive"} onClick={logout}>
+                Logout
+              </Button>
+            </div>
           </div>
         </div>
       </>
@@ -55,40 +57,46 @@ export default function Auth() {
 
   return (
     <>
-      <Header />
-      <div className="flex items-center justify-center h-screen">
-        <div className="flex flex-col gap-y-2 w-100">
-          <h2 className="text-xl bg-yellow-300/40 px-1.5 mx-auto font-mono">
-            {action === "login" ? "Login" : "Register"}
-          </h2>
-          {action === "register" ? (
+      <div className="max-w-[800px] mx-auto pt-5">
+        <Header />
+        <div className="flex items-center justify-center h-screen">
+          <div className="flex flex-col gap-y-2 w-100">
+            <h2 className="text-xl bg-yellow-300/40 px-1.5 mx-auto font-mono">
+              {action === "login" ? "Login" : "Register"}
+            </h2>
+            {action === "register" ? (
+              <Input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Email"
+              />
+            ) : null}
             <Input
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              placeholder="Email"
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              placeholder="User Name"
             />
-          ) : null}
-          <Input
-            onChange={(e) => setUsername(e.target.value)}
-            type="text"
-            placeholder="User Name"
-          />
-          <Input
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            placeholder="Password"
-          />
-          <Button type="submit" onClick={handlerSubmit}>
-            {action === "login" ? "Login" : "Register"}
-          </Button>
-          <Button variant={"link"} type="button" onClick={handlerChangeAction}>
-            {action === "login"
-              ? "Don't have an account? Register"
-              : "Already have an account? Login"}
-          </Button>
-          {errorMessage ? (
-            <p className="text-red-500 text-sm text-center">{errorMessage}</p>
-          ) : null}
+            <Input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password"
+            />
+            <Button type="submit" onClick={handlerSubmit}>
+              {action === "login" ? "Login" : "Register"}
+            </Button>
+            <Button
+              variant={"link"}
+              type="button"
+              onClick={handlerChangeAction}
+            >
+              {action === "login"
+                ? "Don't have an account? Register"
+                : "Already have an account? Login"}
+            </Button>
+            {errorMessage ? (
+              <p className="text-red-500 text-sm text-center">{errorMessage}</p>
+            ) : null}
+          </div>
         </div>
       </div>
     </>
