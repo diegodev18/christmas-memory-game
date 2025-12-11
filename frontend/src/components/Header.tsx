c184757d91818cb60591de77b406cc56f8fce3e6
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-12-10 22:14:39 -0600

    feat: add built with information and repository link to Header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,6 +7,18 @@ export default function Header() {
 
   return (
     <header className="flex justify-end h-9 gap-x-4">
+      <p className="fixed top-2 left-2 text-sm opacity-85">
+        Built with ❤️ by k014 on{" "}
+        <a
+          className="hover:underline text-blue-300"
+          href="https://github.com/diegodev18/christmas-memory-game"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Repository
+        </a>
+        .
+      </p>
       <Button onClick={() => navigate("/scoreboard")} variant={"secondary"}>
         Scoreboard
       </Button>
